dcb636aaccab20e93cd228be1424200cb4d09e9e
Vivek rao <vivek@gkpwebsolutions.com>
2022-03-13 13:25:21 +0530

    fixed

--- index.php
+++ index.php
@@ -86,150 +86,8 @@
     <div id="preloader"></div>
 
     <div id="main-wrapper" class="main-wrapper">
-      <header class="header axil-header header-style-1">
-        <div id="axil-sticky-placeholder"></div>
-        <div class="axil-mainmenu">
-          <div class="container">
-            <div class="header-navbar">
-              <div class="header-logo">
-                <a href="index.html"
-                  ><img
-                    class="light-version-logo"
-                    src="assets/media/unified-infotech.svg"
-                    alt="logo"
-                    style="height: 70px"
-                /></a>
-                <a href="index.html"
-                  ><img
-                    class="dark-version-logo"
-                    src="assets/media/unified-infotech.svg"
-                    alt="logo"
-                /></a>
-                <a href="index.html"
-                  ><img
-                    class="sticky-logo"
-                    src="assets/media/float-logo.svg"
-                    alt="logo"
-                    style="height: 70px"
-                /></a>
-              </div>
-              <div class="header-main-nav">
-                <nav class="mainmenu-nav" id="mobilemenu-popup">
-                  <div class="d-block d-lg-none">
-                    <div class="mobile-nav-header">
-                      <div class="mobile-nav-logo">
-                        <a href="index.html">
-                          <img
-                            class="light-mode"
-                            src="assets/media/float-logo.svg"
-                            alt="Site Logo"
-                          />
-                          <img
-                            class="dark-mode"
-                            src="assets/media/float-logo.svg"
-                            alt="Site Logo"
-                          />
-                        </a>
-                      </div>
-                      <button
-                        class="mobile-menu-close"
-                        data-bs-dismiss="offcanvas"
-                      >
-                        <i class="fas fa-times"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <ul class="mainmenu">
-                    <li><a href="index.html">Home</a></li>
-
-                    <li class="menu-item-has-children">
-                      <a href="#">Business Services</a>
-                      <ul class="axil-submenu">
-                        <li>
-                          <a href="service-business-consulting.html"
-                            >Business Consulting</a
-                          >
-                        </li>
-                        <li>
-                          <a href="service-process-automation.html"
-                            >Process Automation</a
-                          >
-                        </li>
-                        <li>
-                          <a href="service-business-continuity.html"
-                            >Business Continuity</a
-                          >
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="menu-item-has-children">
-                      <a href="#">Digital Services</a>
-                      <ul class="axil-submenu">
-                        <li><a href="service-design.html">UX/UI Design</a></li>
-                        <li>
-                          <a href="service-psd-to-html.html"
-                            >Front End Development</a
-                          >
-                        </li>
-                        <li>
-                          <a href="service-development.html"
-                            >Web & Mobile App Development</a
-                          >
-                        </li>
-                        <li>
-                          <a href="cloud-Services.html"> Cloud Services </a>
-                        </li>
-                        <li>
-                          <a href="service-digital-marketing.html"
-                            >Digital Marketing</a
-                          >
-                        </li>
-                      </ul>
-                    </li>
-
-                    <li class="menu-item-has-children">
-                      <a href="javascript:void(0);">Company</a>
-                      <ul class="axil-submenu">
-                        <li><a href="about-us.html">About Us</a></li>
-
-                        <li><a href="our-clients.html">Our Clients</a></li>
-                      </ul>
-                    </li>
-
-                    <li><a href="contact.html">Contact Us</a></li>
-                  </ul>
-                </nav>
-              </div>
-              <div class="header-action">
-                <ul class="list-unstyled">
-                  <li class="sidemenu-btn d-lg-block d-none">
-                    <button
-                      class="btn-wrap"
-                      data-bs-toggle="offcanvas"
-                      data-bs-target="#offcanvasMenuRight"
-                    >
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </button>
-                  </li>
-                  <li class="mobile-menu-btn sidemenu-btn d-lg-none d-block">
-                    <button
-                      class="btn-wrap"
-                      data-bs-toggle="offcanvas"
-                      data-bs-target="#mobilemenu-popup"
-                    >
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      
+    <?php include('nav.php'); ?>
 
       <section class="banner banner-style-1">
         <div class="container">
@@ -1302,7 +1160,7 @@
             <input
               type="text"
               class="form-control"
-              name="contact-name"
+              name="name"
               placeholder="John Smith"
             />
           </div>
@@ -1311,7 +1169,7 @@
             <input
               type="email"
               class="form-control"
-              name="contact-email"
+              name="email"
               placeholder="example@mail.com"
             />
           </div>
@@ -1320,7 +1178,7 @@
             <input
               type="tel"
               class="form-control"
-              name="contact-company"
+              name="number"
               placeholder="+123456789"
             />
           </div>
---
+++ nav.php
@@ -0,0 +1,144 @@
+<header class="header axil-header header-style-1">
+        <div id="axil-sticky-placeholder"></div>
+        <div class="axil-mainmenu">
+          <div class="container">
+            <div class="header-navbar">
+              <div class="header-logo">
+                <a href="index.html"
+                  ><img
+                    class="light-version-logo"
+                    src="assets/media/unified-infotech.svg"
+                    alt="logo"
+                    style="height: 70px"
+                /></a>
+                <a href="index.html"
+                  ><img
+                    class="dark-version-logo"
+                    src="assets/media/unified-infotech.svg"
+                    alt="logo"
+                /></a>
+                <a href="index.html"
+                  ><img
+                    class="sticky-logo"
+                    src="assets/media/float-logo.svg"
+                    alt="logo"
+                    style="height: 70px"
+                /></a>
+              </div>
+              <div class="header-main-nav">
+                <nav class="mainmenu-nav" id="mobilemenu-popup">
+                  <div class="d-block d-lg-none">
+                    <div class="mobile-nav-header">
+                      <div class="mobile-nav-logo">
+                        <a href="index.html">
+                          <img
+                            class="light-mode"
+                            src="assets/media/float-logo.svg"
+                            alt="Site Logo"
+                          />
+                          <img
+                            class="dark-mode"
+                            src="assets/media/float-logo.svg"
+                            alt="Site Logo"
+                          />
+                        </a>
+                      </div>
+                      <button
+                        class="mobile-menu-close"
+                        data-bs-dismiss="offcanvas"
+                      >
+                        <i class="fas fa-times"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <ul class="mainmenu">
+                    <li><a href="index.html">Home</a></li>
+
+                    <li class="menu-item-has-children">
+                      <a href="#">Business Services</a>
+                      <ul class="axil-submenu">
+                        <li>
+                          <a href="service-business-consulting.html"
+                            >Business Consulting</a
+                          >
+                        </li>
+                        <li>
+                          <a href="service-process-automation.html"
+                            >Process Automation</a
+                          >
+                        </li>
+                        <li>
+                          <a href="service-business-continuity.html"
+                            >Business Continuity</a
+                          >
+                        </li>
+                      </ul>
+                    </li>
+                    <li class="menu-item-has-children">
+                      <a href="#">Digital Services</a>
+                      <ul class="axil-submenu">
+                        <li><a href="service-design.html">UX/UI Design</a></li>
+                        <li>
+                          <a href="service-psd-to-html.html"
+                            >Front End Development</a
+                          >
+                        </li>
+                        <li>
+                          <a href="service-development.html"
+                            >Web & Mobile App Development</a
+                          >
+                        </li>
+                        <li>
+                          <a href="cloud-Services.html"> Cloud Services </a>
+                        </li>
+                        <li>
+                          <a href="service-digital-marketing.html"
+                            >Digital Marketing</a
+                          >
+                        </li>
+                      </ul>
+                    </li>
+
+                    <li class="menu-item-has-children">
+                      <a href="javascript:void(0);">Company</a>
+                      <ul class="axil-submenu">
+                        <li><a href="about-us.html">About Us</a></li>
+
+                        <li><a href="our-clients.html">Our Clients</a></li>
+                      </ul>
+                    </li>
+
+                    <li><a href="contact.html">Contact Us</a></li>
+                  </ul>
+                </nav>
+              </div>
+              <div class="header-action">
+                <ul class="list-unstyled">
+                  <li class="sidemenu-btn d-lg-block d-none">
+                    <button
+                      class="btn-wrap"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#offcanvasMenuRight"
+                    >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </button>
+                  </li>
+                  <li class="mobile-menu-btn sidemenu-btn d-lg-none d-block">
+                    <button
+                      class="btn-wrap"
+                      data-bs-toggle="offcanvas"
+                      data-bs-target="#mobilemenu-popup"
+                    >
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
